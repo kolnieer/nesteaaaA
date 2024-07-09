51917b6ea62219c5c80159f04441751bbcb1ada9
kolnieer <tolentinoannenicol@gmail.com>
2024-07-09 15:40:34 +0800

    activity 5

--- backstore/src/main/java/com/nesteaa/backstore/controllers/UserAuthController.java
+++ backstore/src/main/java/com/nesteaa/backstore/controllers/UserAuthController.java
@@ -3,15 +3,20 @@ package com.nesteaa.backstore.controllers;
 import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nesteaa.backstore.DTO.LoginRequest;
 import com.nesteaa.backstore.DTO.RegistrationRequest;
 import com.nesteaa.backstore.Model.Role;
 import com.nesteaa.backstore.Model.UserAuth;
@@ -88,5 +93,22 @@ public class UserAuthController {
         userAuthRepository.save(userAuth);
 
         return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest){
+        try{
+            Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                    loginRequest.getUsernameOrEmail(),
+                    loginRequest.getPassword()
+                    )
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            return new ResponseEntity<>("User logged in successfully", HttpStatus.OK);
+        }catch(Exception e){
+            return new ResponseEntity<>("Invalid username or password", HttpStatus.UNAUTHORIZED);
+        }
     }
 }
